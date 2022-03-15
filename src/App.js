93493f87bd1558155c4f7354f7bc079f9e368230
Jovana Ranđelović <jovana@stuntcoders.com>
2022-03-15 12:07:16 +0100

    Update app component by adding routes/subroutes for admin/candidate sections
    Update code style

--- src/App.js
+++ src/App.js
@@ -3,11 +3,22 @@ import { Routes, Route } from "react-router-dom"
 import Login from "./components/Login"
 import NotFound from "./components/NotFound"
 
+import HomeAdmin from "./components/admin/Home"
+import Tests from "./components/admin/Tests"
+import Users from "./components/admin/Users"
+
+import HomeCandidate from "./components/candidate/Home"
+
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={ <Login/> } />
+        <Route path="/admin" element={ <HomeAdmin /> } >
+          <Route path="tests" element={<Tests />} />
+          <Route path="users" element={<Users />} />
+        </Route>
+        <Route path="/candidate" element={ <HomeCandidate /> } />
         <Route path="*" element={ <NotFound /> } />
       </Routes>
     </>
