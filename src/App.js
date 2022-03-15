724fab3791c25cc1cd3d2633786eb23582ed89b9
Jovana Ranđelović <jovana@stuntcoders.com>
2022-03-15 10:26:33 +0100

    Add basic routes for login/notfound

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
+import { Routes, Route } from "react-router-dom"
+
+import Login from "./components/Login"
+import NotFound from "./components/NotFound"
+
 function App() {
   return (
-    <></>
+    <>
+      <Routes>
+        <Route path="/" element={ <Login/> } />
+        <Route path="*" element={ <NotFound /> } />
+      </Routes>
+    </>
   );
 }
 
