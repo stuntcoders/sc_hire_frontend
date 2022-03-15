9b9224c6e630d4f5b8d582bb6c7929d5b92a3025
Jovana Ranđelović <jovana@stuntcoders.com>
2022-03-15 15:08:14 +0100

    Update admin users component by adding set tab state to display user forms by type

--- src/components/admin/Users.js
+++ src/components/admin/Users.js
@@ -1,7 +1,38 @@
+import { useState } from "react";
+
+import ShowUsers from "./ShowUsers"
+
 export default function Users() {
-  return(
-    <div>
-      Hello from Users
-    </div>
-  )
+  const [tab, setTabState] = useState(0);
+
+  const showTab = index => {
+    setTabState(index);
+  };
+
+  return (
+    <>
+      <ul className="flex mb-2 space-x-2">
+        <li className={ `flex items-end ${tab === 0 ? "text-3xl" : "text-2xl text-gray-400"}` }
+            onClick={() => showTab(0)}
+        >
+          <span className="cursor-pointer">
+            Admin
+          </span>
+          <span className="inline-flex self-center w-20 h-0.5 ml-2 mt-2 bg-indigo-500"></span>
+        </li>
+        <li className={ `flex items-end ${tab === 1 ? "text-3xl" : "text-2xl text-gray-400"}` }
+            onClick={() => showTab(1)}
+        >
+          <span className="cursor-pointer">
+            Candidates
+          </span>
+        </li>
+      </ul>
+
+      <div>
+        <ShowUsers type="Admin" className={tab === 0 ? "block" : "hidden"} />
+        <ShowUsers type="Candidate" className={tab === 1 ? "block" : "hidden"} />
+      </div>
+    </>
+  );
 }
