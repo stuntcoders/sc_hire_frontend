4520b5acc1ea6e0e707295ce68733f1f3a6ef590
Jovana Ranđelović <jovana@stuntcoders.com>
2022-03-16 14:40:30 +0100

    Update style for theme toggle component

--- src/components/ThemeToggle.js
+++ src/components/ThemeToggle.js
@@ -2,14 +2,14 @@ import { SunIcon, MoonIcon } from "@heroicons/react/outline"
 
 export default function ThemeToggle({ theme, setTheme }) {
   return (
-    <div className="absolute bottom-6 right-6 p-1 bg-gray-100 dark:bg-indigo-600 shadow-sm rounded-full">
-      <span>
+    <div className="absolute bottom-6 right-6 p-1 bg-gray-100 hover:bg-indigo-100 dark:bg-indigo-600 dark:hover:bg-indigo-500 shadow-sm rounded-full group">
+      <span className="cursor-pointer">
         { theme === "dark" ?
           <span onClick={() => setTheme(theme)}>
-            <MoonIcon className="h-7 w-7 pointer-events-none text-gray-600" />
+            <MoonIcon className="h-7 w-7 pointer-events-none text-indigo-600 group-hover:text-indigo-700" />
           </span> :
           <span onClick={() => setTheme(theme)}>
-            <SunIcon className="h-7 w-7 pointer-events-none text-gray-50" />
+            <SunIcon className="h-7 w-7 pointer-events-none text-gray-50 group-hover:text-white" />
           </span> }
       </span>
     </div>
