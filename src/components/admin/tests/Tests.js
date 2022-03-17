ebfb43aff77f59cabd9a613a98a104a4911e92f0
Jovana Ranđelović <jovana@stuntcoders.com>
2022-03-17 10:28:14 +0100

    Update admin tests template by adding tab section

--- src/components/admin/tests/Tests.js
+++ src/components/admin/tests/Tests.js
@@ -1,7 +1,27 @@
+import useTab from "../../../hooks/useTab";
+
 export default function Tests() {
+  const [itemTabOpen, toggleTab] = useTab()
+
   return(
-    <div>
-      Hello from Tests
-    </div>
+    <>
+      <ul className="flex mb-2 space-x-2">
+        <li className={ `flex items-end ${itemTabOpen === 0 ? "text-3xl font-light text-gray-800 dark:text-gray-50" : "text-2xl font-extralight text-gray-400 dark:text-gray-500"}` }
+            onClick={() => toggleTab(0)}
+        >
+          <span className="cursor-pointer">
+            Template
+          </span>
+          <span className="inline-flex self-center w-16 h-px ml-2 mt-2 bg-indigo-500"></span>
+        </li>
+        <li className={ `flex items-end ${itemTabOpen === 1 ? "text-3xl font-light text-gray-800 dark:text-gray-50" : "text-2xl font-extralight text-gray-400 dark:text-gray-500"}` }
+            onClick={() => toggleTab(1)}
+        >
+          <span className="cursor-pointer">
+            Assigned
+          </span>
+        </li>
+      </ul>
+    </>
   )
 }
