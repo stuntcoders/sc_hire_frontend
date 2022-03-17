e63df3c269961da9ea171d8daf2b4c3421abff86
Jovana Ranđelović <jovana@stuntcoders.com>
2022-03-17 15:19:10 +0100

    Minor style update for user form component

--- src/components/admin/users/UserForm.js
+++ src/components/admin/users/UserForm.js
@@ -2,7 +2,7 @@ import { ReactComponent as PolygonLeft } from "../../../assets/shapes/polygon_le
 
 export default function UserForm({ user }) {
   return (
-    <div className="relative bg-white dark:bg-gray-50 shadow mx-4 mt-4 px-4 py-5 sm:rounded-lg sm:p-6 z-10 overflow-hidden">
+    <div className={`relative bg-white dark:bg-gray-50 shadow mx-4 px-4 py-5 sm:rounded-lg sm:p-6 z-10 overflow-hidden ${user.id ? "mt-1" : "mt-4"}`}>
       {user.id && <PolygonLeft fill="#4338ca" className="absolute top-0 left-0 hidden md:block" /> }
 
       <div className="relative md:grid md:grid-cols-3 md:gap-6 z-10">
