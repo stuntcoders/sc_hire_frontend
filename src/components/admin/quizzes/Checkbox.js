2b39f15007e9d846666416830e271e6e9da3f835
Jovana Ranđelović <jovana@stuntcoders.com>
2022-03-18 16:35:30 +0100

    Update checkbox component with appropriate inputs for checked answer/answer text

--- src/components/admin/quizzes/Checkbox.js
+++ src/components/admin/quizzes/Checkbox.js
@@ -1,5 +1,24 @@
 export default function Checkbox({ index }) {
   return (
-    <div>{ index }</div>
+    <fieldset className="space-y-4 pl-2">
+      <div className="relative flex items-start">
+        <div className="flex items-center h-5">
+          <input
+            id="candidates"
+            aria-describedby="candidates-description"
+            name="candidates"
+            type="checkbox"
+            className="mt-1 focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300 rounded"
+          />
+        </div>
+        <div className="ml-4 border-b border-gray-300 focus-within:border-indigo-600">
+          <input
+            type="text"
+            className="inline-block w-full h-6 border-0 border-b border-transparent bg-white focus:border-indigo-600 focus:ring-0 sm:text-sm"
+            placeholder="Checkbox option 1"
+          />
+        </div>
+      </div>
+    </fieldset>
   )
 }
