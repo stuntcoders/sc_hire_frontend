bca312561585a33ba4f93d4058798e47e741448b
Jovana Ranđelović <jovana@stuntcoders.com>
2022-03-18 13:39:52 +0100

    Update code style

--- src/hooks/useQuery.js
+++ src/hooks/useQuery.js
@@ -1,8 +1,8 @@
-import React from "react";
-import { useLocation } from "react-router-dom";
+import React from "react"
+import { useLocation } from "react-router-dom"
 
 export default function useQuery() {
-  const { search } = useLocation();
+  const { search } = useLocation()
 
-  return React.useMemo(() => new URLSearchParams(search), [search]);
+  return React.useMemo(() => new URLSearchParams(search), [search])
 }
