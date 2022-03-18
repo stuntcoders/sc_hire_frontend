2e6d6d0dec7c6917321b2435839fe4fb968e0729
Jovana Ranđelović <jovana@stuntcoders.com>
2022-03-18 15:12:17 +0100

    Update hero/stats components style/content

--- src/components/admin/dashboard/Hero.js
+++ src/components/admin/dashboard/Hero.js
@@ -1,38 +1,11 @@
 export default function Hero() {
   return (
-    <div className="relative bg-gray-50 dark:bg-white overflow-hidden rounded-t">
-      <div className="hidden sm:block sm:absolute sm:inset-y-0 sm:h-full sm:w-full" aria-hidden="true">
-        <div className="relative h-full max-w-7xl mx-auto">
-          <svg
-            className="absolute right-full transform -translate-y-1/4 translate-x-1/4 lg:translate-x-1/2"
-            width={404}
-            height={784}
-            fill="none"
-            viewBox="0 0 404 784"
-          >
-            <defs>
-              <pattern
-                id="f210dbf6-a58d-4871-961e-36d5016a0f49"
-                x={0}
-                y={0}
-                width={20}
-                height={20}
-                patternUnits="userSpaceOnUse"
-              >
-                <rect x={0} y={0} width={4} height={4} className="text-gray-100" fill="currentColor" />
-              </pattern>
-            </defs>
-            <rect width={404} height={784} fill="url(#f210dbf6-a58d-4871-961e-36d5016a0f49)" />
-          </svg>
-        </div>
-      </div>
-
+    <div className="relative bg-gray-50 dark:bg-gray-700 overflow-hidden rounded-t">
       <div className="relative pb-16 sm:pb-24">
         <main className="mt-16 mx-auto max-w-7xl px-4 sm:mt-24">
           <div className="text-center">
-            <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-3xl md:text-4xl">
+            <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-1xl md:text-2xl">
               <span className="block xl:inline">
-                Welcome <span className="text-indigo-600">admin@example.com</span>
               </span>
             </h1>
           </div>
