556e14805503b77a094d760a2d5ed63bbb8cc381
Jovana Ranđelović <jovana@stuntcoders.com>
2022-03-18 13:48:33 +0100

    Add placeholder checkbox/radio quiz components

---
+++ src/components/admin/quizzes/Checkbox.js
@@ -0,0 +1,5 @@
+export default function Checkbox({ index }) {
+  return (
+    <div>{ index }</div>
+  )
+}
---
+++ src/components/admin/quizzes/Radio.js
@@ -0,0 +1,5 @@
+export default function Radio({ index }) {
+  return (
+    <div>{ index }</div>
+  )
+}
