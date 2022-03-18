b5a2643f6a216a91ef830f03ddfc4196229d82a2
Jovana Ranđelović <jovana@stuntcoders.com>
2022-03-18 16:34:56 +0100

    Update question component with input hint for checkbox/radio

--- src/components/admin/quizzes/Question.js
+++ src/components/admin/quizzes/Question.js
@@ -17,14 +17,28 @@ export default function Question({ type, index, removeQuestion }) {
           autoComplete="off"
           className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
         />
+        {(() => {
+          switch (type) {
+            case "checkbox":
+              return <p className="mt-2 text-sm text-gray-500" id="email-description">
+                *Please check correct answer(s)
+              </p>
+            case "radio":
+              return <p className="mt-2 text-sm text-gray-500" id="email-description">
+                *Please check correct answer
+              </p>
+            default:
+              return null
+          }
+        })()}
       </div>
-      <div className="col-span-3 sm:col-span-1 flex justify-start items-end">
+      <div className={`col-span-3 sm:col-span-1 flex justify-start ${type === "checkbox" || type === "radio" ? "items-center" : "items-end" }`}>
         <button
           onClick={() => removeQuestion(index)}
           type="button"
           className="group focus:outline-none mb-2"
         >
-          <TrashIcon className="h-6 w-6 text-gray-800 group-hover:text-indigo-700 pointer-events-none" />
+          <TrashIcon className="h-6 w-6 text-gray-400 group-hover:text-indigo-700 pointer-events-none" />
         </button>
       </div>
       <div className="col-span-3">
