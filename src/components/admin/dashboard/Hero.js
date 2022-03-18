06a0bb500d66db9c8a16107778c157dbcc4784ed
Jovana Ranđelović <jovana@stuntcoders.com>
2022-03-18 14:47:37 +0100

    Update dashboard hero/stats components style

--- src/components/admin/dashboard/Hero.js
+++ src/components/admin/dashboard/Hero.js
@@ -24,27 +24,6 @@ export default function Hero() {
             </defs>
             <rect width={404} height={784} fill="url(#f210dbf6-a58d-4871-961e-36d5016a0f49)" />
           </svg>
-          <svg
-            className="absolute right-0 -top-12 rotate-45"
-            width={404}
-            height={784}
-            fill="none"
-            viewBox="0 0 404 784"
-          >
-            <defs>
-              <pattern
-                id="f210dbf6-a58d-4871-961e-36d5016a0f49"
-                x={0}
-                y={0}
-                width={20}
-                height={20}
-                patternUnits="userSpaceOnUse"
-              >
-                <rect x={0} y={0} width={4} height={4} className="text-gray-100" fill="currentColor" />
-              </pattern>
-            </defs>
-            <rect width={404} height={784} fill="url(#f210dbf6-a58d-4871-961e-36d5016a0f49)" />
-          </svg>
         </div>
       </div>
 
--- src/components/admin/dashboard/Stats.js
+++ src/components/admin/dashboard/Stats.js
@@ -7,8 +7,8 @@ const stats = [
 export default function Stats() {
   return (
     <>
-      <div className="relative bg-gray-50 dark:bg-white p-6 pt-1 z-10">
-        <dl className="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
+      <div className="relative bg-gray-50 dark:bg-white p-6 pt-1 z-10 overflow-hidden">
+        <dl className="relative mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3 z-10">
           {stats.map((item) => (
             <div key={item.name} className="px-4 py-5 bg-white dark:bg-gray-50 shadow rounded-lg overflow-hidden sm:p-6">
               <dt className="text-sm font-medium text-gray-500 truncate">{item.name}</dt>
@@ -16,6 +16,27 @@ export default function Stats() {
             </div>
           ))}
         </dl>
+        <svg
+          className="absolute -right-2 top-6 rotate-[5deg]"
+          width={404}
+          height={784}
+          fill="none"
+          viewBox="0 0 404 784"
+        >
+          <defs>
+            <pattern
+              id="f210dbf6-a58d-4871-961e-36d5016a0f49"
+              x={0}
+              y={0}
+              width={20}
+              height={20}
+              patternUnits="userSpaceOnUse"
+            >
+              <rect x={0} y={0} width={4} height={4} className="text-gray-100" fill="currentColor" />
+            </pattern>
+          </defs>
+          <rect width={404} height={784} fill="url(#f210dbf6-a58d-4871-961e-36d5016a0f49)" />
+        </svg>
       </div>
       <div className="relative hidden sm:block lg:inset-y-0 lg:right-0 mb-6 border-t-4 border-indigo-600 shadow z-10">
         <img
