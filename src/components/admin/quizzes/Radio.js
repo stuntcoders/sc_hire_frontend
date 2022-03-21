38024eb6f2d173c4adaa921e090d3151188aa8fc
Jovana Ranđelović <jovana@stuntcoders.com>
2022-03-21 12:43:41 +0100

    Update radio component to add additional options by utilizing custom hook use question list

--- src/components/admin/quizzes/Radio.js
+++ src/components/admin/quizzes/Radio.js
@@ -1,24 +1,67 @@
+import useQuestionList from "../../../hooks/useQuestionList"
+
+import { PlusSmIcon, MinusSmIcon } from "@heroicons/react/outline"
+
 export default function Radio({ index }) {
+  const [questionList, addQuestion, removeQuestion] = useQuestionList()
+
   return (
-    <fieldset>
-      <div className="space-y-4 pl-2">
-        <div className="flex items-start">
-          <div className="flex items-center h-5">
-            <input
-              id={`radio-${index}`}
-              name={`radio-${index}`}
-              type="radio"
-              className="mt-1.5 focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300"
-            />
-          </div>
-          <div className="ml-4 border-b border-gray-300 focus-within:border-indigo-600">
-            <input
-              type="text"
-              className="inline-block w-full h-6 border-0 border-b border-transparent bg-white focus:border-indigo-600 focus:ring-0 sm:text-sm"
-              placeholder="Radio option 1"
-            />
-          </div>
+    <fieldset className="space-y-4 p-4 bg-gray-50 rounded">
+      <div className="flex items-start">
+        <div className="flex items-center h-5">
+          <input
+            id={`radio-${index}-0`}
+            name={`radio-${index}-0`}
+            type="radio"
+            className="mt-1.5 focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300"
+          />
         </div>
+        <div className="w-full ml-4 border-b border-gray-300 focus-within:border-indigo-600">
+          <input
+            id={`radio-input-${index}-0`}
+            type="text"
+            className="inline-block w-full h-6 border-0 border-b border-transparent bg-gray-50 placeholder:text-gray-300 focus:border-indigo-600 focus:ring-0 sm:text-sm"
+            placeholder="Radio option 1"
+          />
+        </div>
+      </div>
+
+      {questionList.map((question, question_index) => {
+        return (
+          <div key={question_index} className="relative flex items-start bg-gray-50">
+            <div className="flex items-center h-5">
+              <input
+                id={question.radio}
+                name={question.radio}
+                type="radio"
+                className="mt-1 h-4 w-4 text-indigo-600 border-gray-300 focus:ring-indigo-500"
+              />
+            </div>
+            <div className="w-full ml-4 border-b border-gray-300 focus-within:border-indigo-600">
+              <input
+                id={question.input}
+                type="text"
+                className="inline-block w-full h-6 border-0 border-b border-transparent bg-gray-50 placeholder:text-gray-300 focus:border-indigo-600 focus:ring-0 sm:text-sm"
+                placeholder={`Radio option ${question_index + 2}`}
+              />
+            </div>
+          </div>
+        );
+      })}
+
+      <div className="flex col-span-6 sm:col-span-2 justify-center divide-x">
+        <span
+          onClick={() => addQuestion({ radio: `radio-${index}-${questionList.length + 1}`, input: `radio-input-${index}-${questionList.length + 1}` })}
+          className="p-1 bg-indigo-600 hover:bg-indigo-700 rounded-l cursor-pointer"
+        >
+          <PlusSmIcon className="h-5 w-5 text-white pointer-events-none" />
+        </span>
+        <span
+          onClick={() => removeQuestion(-1)}
+          className={`p-1 rounded-r ${ questionList.length ? "bg-indigo-600 hover:bg-indigo-700 cursor-pointer" : "bg-gray-200 cursor-not-allowed" }`}
+        >
+          <MinusSmIcon className="h-5 w-5 text-white pointer-events-none" />
+        </span>
       </div>
     </fieldset>
   )
