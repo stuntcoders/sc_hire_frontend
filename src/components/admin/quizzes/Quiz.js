b3e893012f5f444dbab1ac367583fda318676b90
Jovana Ranđelović <jovana@stuntcoders.com>
2022-03-22 09:29:53 +0100

    Update quiz/review components to animate on render

--- src/components/admin/quizzes/Quiz.js
+++ src/components/admin/quizzes/Quiz.js
@@ -1,38 +1,42 @@
+import AnimatedPage from "../../AnimatedPage"
+
 import { Link } from "react-router-dom"
 
 import { CollectionIcon, ChevronRightIcon } from "@heroicons/react/outline"
 
 export default function Quiz({ quiz }) {
   return (
-    <li key={quiz.id}>
-      <div className="block bg-gray-50 dark:bg-white">
-        <div className="px-4 py-4 flex items-center sm:px-6">
-          <div className="min-w-0 flex-1 sm:flex sm:items-center sm:justify-between">
-            <div className="truncate">
-              <div className="flex text-sm">
-                <p className="font-medium truncate">{quiz.title}</p>
-                <span className="inline-flex self-center w-10 mt-0.5 mx-1 h-px bg-gray-300"></span>
-                <p className="ml-0.5 text-indigo-500">
-                  {quiz.duration} minutes
-                </p>
-              </div>
-              <div className="mt-2 flex">
-                <div className="flex items-center text-sm text-gray-500">
-                  <CollectionIcon className="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400" aria-hidden="true" />
-                  <span className="font-light">
-                    {quiz.questions.length} question(s)
-                  </span>
+    <AnimatedPage>
+      <li key={quiz.id}>
+        <div className="block bg-gray-50 dark:bg-white">
+          <div className="px-4 py-4 flex items-center sm:px-6">
+            <div className="min-w-0 flex-1 sm:flex sm:items-center sm:justify-between">
+              <div className="truncate">
+                <div className="flex text-sm">
+                  <p className="font-medium truncate">{quiz.title}</p>
+                  <span className="inline-flex self-center w-10 mt-0.5 mx-1 h-px bg-gray-300"></span>
+                  <p className="ml-0.5 text-indigo-500">
+                    {quiz.duration} minutes
+                  </p>
+                </div>
+                <div className="mt-2 flex">
+                  <div className="flex items-center text-sm text-gray-500">
+                    <CollectionIcon className="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400" aria-hidden="true" />
+                    <span className="font-light">
+                      {quiz.questions.length} question(s)
+                    </span>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-          <div className="ml-5 flex-shrink-0 cursor-pointer">
-            <Link to={`${quiz.id}`}>
-              <ChevronRightIcon className="h-5 w-5 text-gray-400 pointer-events-none" aria-hidden="true" />
-            </Link>
+            <div className="ml-5 flex-shrink-0 cursor-pointer">
+              <Link to={`${quiz.id}`}>
+                <ChevronRightIcon className="h-5 w-5 text-gray-400 pointer-events-none" aria-hidden="true" />
+              </Link>
+            </div>
           </div>
         </div>
-      </div>
-    </li>
+      </li>
+    </AnimatedPage>
   )
 }
--- src/components/admin/quizzes/Review.js
+++ src/components/admin/quizzes/Review.js
@@ -1,57 +1,61 @@
+import AnimatedPage from "../../AnimatedPage"
+
 import { Link } from "react-router-dom"
 
 import { ClockIcon, ChevronRightIcon } from "@heroicons/react/outline"
 
 export default function Review({ review }) {
   return (
-    <li key={review.id}>
-      <section className="block bg-gray-50 dark:bg-white">
-        <div className="px-4 py-4 flex items-center sm:px-6">
-          <div className="min-w-0 flex-1 sm:flex sm:items-center sm:justify-between">
-            <div className="truncate">
-              <div className="flex">
-                <p className="text-sm font-medium text-indigo-600 truncate">{review.id}</p>
+    <AnimatedPage>
+      <li key={review.id}>
+        <section className="block bg-gray-50 dark:bg-white">
+          <div className="px-4 py-4 flex items-center sm:px-6">
+            <div className="min-w-0 flex-1 sm:flex sm:items-center sm:justify-between">
+              <div className="truncate">
+                <div className="flex">
+                  <p className="text-sm font-medium text-indigo-600 truncate">{review.id}</p>
+                </div>
+                <div className="mt-2 flex">
+                  <div className="flex items-center text-sm text-gray-500">
+                    <ClockIcon className="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400" aria-hidden="true" />
+                    <span className="truncate">
+                      {new Intl.DateTimeFormat("default", {
+                        year: "numeric",
+                        month: "long",
+                        day: "2-digit",
+                        hour: "numeric",
+                        minute: "numeric"
+                      }).format(review.start)}
+                    </span>
+                    &nbsp;-&nbsp;
+                    <span className="truncate">
+                      {new Intl.DateTimeFormat("default", {
+                        year: "numeric",
+                        month: "long",
+                        day: "2-digit",
+                        hour: "numeric",
+                        minute: "numeric"
+                      }).format(review.end)}
+                    </span>
+                  </div>
+                </div>
               </div>
-              <div className="mt-2 flex">
-                <div className="flex items-center text-sm text-gray-500">
-                  <ClockIcon className="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400" aria-hidden="true" />
-                  <span className="truncate">
-                    {new Intl.DateTimeFormat("default", {
-                      year: "numeric",
-                      month: "long",
-                      day: "2-digit",
-                      hour: "numeric",
-                      minute: "numeric"
-                    }).format(review.start)}
-                  </span>
-                  &nbsp;-&nbsp;
-                  <span className="truncate">
-                    {new Intl.DateTimeFormat("default", {
-                      year: "numeric",
-                      month: "long",
-                      day: "2-digit",
-                      hour: "numeric",
-                      minute: "numeric"
-                    }).format(review.end)}
+              <div className="mt-4 flex-shrink-0 sm:mt-0 sm:ml-5">
+                <div className="flex overflow-hidden -space-x-1">
+                  <span className={`text-sm ${review.status === "passed" ? "text-indigo-600" : "text-gray-300"}`}>
+                    {review.status}
                   </span>
                 </div>
               </div>
             </div>
-            <div className="mt-4 flex-shrink-0 sm:mt-0 sm:ml-5">
-              <div className="flex overflow-hidden -space-x-1">
-                <span className={`text-sm ${review.status === "passed" ? "text-indigo-600" : "text-gray-300"}`}>
-                  {review.status}
-                </span>
-              </div>
+            <div className="ml-5 flex-shrink-0 cursor-pointer">
+              <Link to={`/admin/review/${review.id}`}>
+                <ChevronRightIcon className="h-5 w-5 text-gray-400 pointer-events-none" aria-hidden="true" />
+              </Link>
             </div>
           </div>
-          <div className="ml-5 flex-shrink-0 cursor-pointer">
-            <Link to={`/admin/review/${review.id}`}>
-              <ChevronRightIcon className="h-5 w-5 text-gray-400 pointer-events-none" aria-hidden="true" />
-            </Link>
-          </div>
-        </div>
-      </section>
-    </li>
+        </section>
+      </li>
+    </AnimatedPage>
   )
 }
