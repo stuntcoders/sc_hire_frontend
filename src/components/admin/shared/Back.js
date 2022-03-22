5147d668fb4ba843c58de231a64d7475799f1316
Jovana Ranđelović <jovana@stuntcoders.com>
2022-03-22 13:24:35 +0100

    Update back component with custom motion animations

--- src/components/admin/shared/Back.js
+++ src/components/admin/shared/Back.js
@@ -1,15 +1,38 @@
+import { motion } from "framer-motion"
+
 import { Link } from "react-router-dom"
 
 import { ChevronLeftIcon } from "@heroicons/react/outline"
+
+const animations = {
+  rest: { x: -1 },
+  hover: { x: [ -4, -6, -2, -4, -6, -2, 0],
+    transition: {
+      duration: 1,
+      ease: "linear"
+    }
+  }
+};
 
 export default function Back({ path, search = "" }) {
   return (
     <ul className="flex mb-2 space-x-2">
       <li className="flex items-end text-2xl font-extralight text-gray-400 dark:text-gray-500">
         <Link to={{ pathname: `${path}`, search: `${search}` }}>
-          <span className="flex items-center group hover:text-indigo-500 cursor-pointer">
-            <ChevronLeftIcon className="inline-block mt-px h-5 w-5 text-gray-500 group-hover:text-indigo-500" /> Back
-          </span>
+          <motion.span
+            className="flex items-center group hover:text-indigo-500 cursor-pointer"
+            initial="rest"
+            whileHover="hover"
+            animate="rest"
+          >
+            <motion.span
+              className="flex items-center"
+              variants={animations}
+            >
+              <ChevronLeftIcon className="inline-block mt-px h-5 w-5 text-gray-500 group-hover:text-indigo-500" />
+            </motion.span>
+            <motion.span>Back</motion.span>
+          </motion.span>
         </Link>
       </li>
     </ul>
