a57fc7a5a33e43198d0f089fab7b64f74abc341e
Jovana Ranđelović <jovana@stuntcoders.com>
2022-03-22 15:24:00 +0100

    Update question component switch statement

--- src/components/admin/quizzes/Question.js
+++ src/components/admin/quizzes/Question.js
@@ -6,6 +6,21 @@ import Radio from "./Radio"
 import { XIcon } from "@heroicons/react/outline"
 
 export default function Question({ type, index, removeQuestion }) {
+  const instructions = (inputType) => {
+    switch (inputType) {
+      case "checkbox":
+        return <p className="mt-2 text-sm text-gray-500 dark:text-gray-300 italic" id="email-description">
+          *Please check correct answer(s)
+        </p>
+      case "radio":
+        return <p className="mt-2 text-sm text-gray-500 dark:text-gray-300 italic" id="email-description">
+          *Please check correct answer
+        </p>
+      default:
+        return null
+    }
+  }
+
   return (
     <>
       <div className={`col-span-3 w-full bg-gray-50 dark:bg-gray-800 p-4 border-t-2 border-gray-200 dark:border-gray-600 ${ (type === "text" || type === "code") ? "rounded" : "rounded-t"}`}>
@@ -27,20 +42,7 @@ export default function Question({ type, index, removeQuestion }) {
           autoComplete="off"
           className="mt-1 dark:bg-gray-700 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm dark:text-white border-gray-300 dark:border-none rounded-md"
         />
-        {(() => {
-          switch (type) {
-            case "checkbox":
-              return <p className="mt-2 text-sm text-gray-500 dark:text-gray-300 italic" id="email-description">
-                *Please check correct answer(s)
-              </p>
-            case "radio":
-              return <p className="mt-2 text-sm text-gray-500 dark:text-gray-300 italic" id="email-description">
-                *Please check correct answer
-              </p>
-            default:
-              return null
-          }
-        })()}
+        {instructions(type)}
       </div>
       <div className="col-span-3 mb-4 shadow-sm">
         {(() => {
