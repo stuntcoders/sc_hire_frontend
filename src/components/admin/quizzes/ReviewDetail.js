9634a1187a97e7c58e9bf3b8b83fcf50f1b75f6b
Jovana Ranđelović <jovana@stuntcoders.com>
2022-03-23 11:39:15 +0100

    Update review detail component by adding user section to general info
    Update dark theme styles

--- src/components/admin/quizzes/ReviewDetail.js
+++ src/components/admin/quizzes/ReviewDetail.js
@@ -1,24 +1,36 @@
 import AnimatedPage from "../../AnimatedPage"
+import { motion } from "framer-motion"
+
+import useCard from "../../../hooks/useCard"
 
 import { useParams } from "react-router-dom"
 
 import Back from "../shared/Back"
 
 import {
-  DuplicateIcon,
-  CollectionIcon,
-  ClockIcon,
+  AtSymbolIcon,
+  ChevronUpIcon,
   ClipboardCopyIcon,
+  ClockIcon,
+  CollectionIcon,
   CubeIcon,
   CubeTransparentIcon,
+  DuplicateIcon,
   ScissorsIcon,
   TemplateIcon,
+  UserIcon,
 } from "@heroicons/react/outline"
 
 import { reviews } from "../dummyData"
 
+const variants = {
+  open: { rotate: 180 },
+  closed: { rotate: 0 },
+}
+
 export default function ReviewDetails() {
   const { id } = useParams()
+  const [itemCardOpen, toggleCard] = useCard()
 
   return (
     <>
@@ -69,34 +81,63 @@ export default function ReviewDetails() {
                         <span className="font-light dark:text-gray-400">Idle Outside - 0 mins</span>
                       </div>
                     </div>
+                    <div className="space-y-1 py-2.5">
+                      <div className="flex items-center text-sm font-medium text-gray-800 bg-gray-50 dark:bg-gray-700 p-2 rounded">
+                        <span className="ml-1 dark:text-white">User Info</span>
+                        <motion.div
+                          onClick={() => toggleCard()}
+                          className="ml-2 flex-shrink-0 cursor-pointer"
+                          animate={itemCardOpen ? "open" : "closed"}
+                          variants={variants}
+                          whileHover={{ scale: 1.1 }}>
+                          <ChevronUpIcon className="h-5 w-5 text-gray-400 pointer-events-none" aria-hidden="true" />
+                        </motion.div>
+                      </div>
+                      <div>
+                        {itemCardOpen && (
+                          <div className="bg-gray-50 dark:bg-gray-700 p-2 rounded">
+                            <div className="flex items-center text-sm text-gray-500">
+                              <UserIcon className="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400 dark:text-gray-500" aria-hidden="true" />
+                              <span className="font-light dark:text-gray-300">
+                                {reviews[0].user.first_name} {reviews[0].user.last_name}
+                              </span>
+                            </div>
+                            <div className="flex items-center text-sm text-gray-500">
+                              <AtSymbolIcon className="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400 dark:text-gray-500" aria-hidden="true" />
+                              <span className="font-light dark:text-gray-300">{reviews[0].user.email}</span>
+                            </div>
+                          </div>
+                        )}
+                      </div>
+                    </div>
                   </div>
                 </div>
                 <div className="flex justify-center md:justify-start mt-2 md:mt-20">
                   <span className="relative z-0 inline-flex shadow-sm rounded-md">
                     <button
                       type="button"
-                      className={`relative inline-flex items-center px-4 py-2 rounded-l-md border text-sm font-medium focus:z-10 focus:outline-none ${
+                      className={`relative inline-flex items-center px-4 py-2 rounded-l-md border text-sm text-gray-700 dark:text-white font-medium focus:z-10 focus:outline-none ${
                         reviews[2].status === "passed"
                           ? "bg-green-300 border-green-300 text-white"
-                          : "bg-white hover:bg-gray-50 border-gray-300 text-gray-700 focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500"
+                          : "bg-white dark:bg-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600 border-gray-300 dark:border-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500"
                       }`}>
                       Pass
                     </button>
                     <button
                       type="button"
-                      className={`-ml-px relative inline-flex items-center px-4 py-2 border text-sm font-medium text-gray-700 focus:z-10 focus:outline-none ${
+                      className={`-ml-px relative inline-flex items-center px-4 py-2 border text-sm font-medium text-gray-700 dark:text-white focus:z-10 focus:outline-none ${
                         reviews[2].status === "unrevised"
                           ? "bg-indigo-700 border-indigo-700 text-white"
-                          : "bg-white hover:bg-gray-50 border-gray-300 text-gray-700 focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500"
+                          : "bg-white dark:bg-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600 border-gray-300 dark:border-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500"
                       }`}>
                       Unrevised
                     </button>
                     <button
                       type="button"
-                      className={`-ml-px relative inline-flex items-center px-4 py-2 rounded-r-md border text-sm font-medium text-gray-700 focus:z-10 focus:outline-none ${
+                      className={`-ml-px relative inline-flex items-center px-4 py-2 rounded-r-md border text-sm font-medium text-gray-700 dark:text-white focus:z-10 focus:outline-none ${
                         reviews[2].status === "failed"
                           ? "bg-gray-300 border-gray-300 text-white cursor-default"
-                          : "bg-white hover:bg-gray-50 border-gray-300 text-gray-700 focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500"
+                          : "bg-white dark:bg-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600 border-gray-300 dark:border-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500"
                       }`}>
                       Fail
                     </button>
