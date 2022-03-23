0cfe508f14029373211c0b63bda1031f64c508f5
Jovana Ranđelović <jovana@stuntcoders.com>
2022-03-23 15:19:59 +0100

    Update hero component with a search component

--- src/components/admin/dashboard/Hero.js
+++ src/components/admin/dashboard/Hero.js
@@ -1,3 +1,4 @@
+import Search from "./Search"
 import Stats from "./Stats"
 
 export default function Hero() {
@@ -5,12 +6,10 @@ export default function Hero() {
     <div className="relative bg-gray-50 dark:bg-gray-700 overflow-hidden rounded-t">
       <div className="absolute -top-40 -right-96 w-screen h-screen md:w-full bg-indigo-600 dark:bg-gray-600 -rotate-[74deg] transition-colors duration-1000"></div>
 
-      <div className="relative pb-16 sm:pb-24">
-        <main className="mt-16 mx-auto max-w-7xl px-4 sm:mt-24">
-          <div className="text-center">
-            <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-1xl md:text-2xl">
-              <span className="block xl:inline"></span>
-            </h1>
+      <div className="relative pt-2 pb-16 sm:pb-24">
+        <main className="mt-2 mx-auto max-w-7xl px-4">
+          <div className="md:w-2/3 px-2">
+            <Search />
           </div>
         </main>
       </div>
